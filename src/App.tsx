d610c8c146639b8de0f295b9305ed22139f2c1f8
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2025-06-18 20:06:17 +0100

    refactor: modify project to use localStorage theme and ResponsiveDrawer Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { Box, ThemeProvider, Typography } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
-import { Header } from './components/Header';
+import { Box, Typography } from '@mui/material';
+
 import { Layout } from './components/Layout';
-import { appTheme } from './config/theme';
 import { ListCategory } from './features/categories/ListCategory';
 import { CreateCategory } from './features/categories/CreateCategory';
 import { EditCategory } from './features/categories/EditCategory';
-import { SnackbarProvider } from 'notistack';
 import { ListCastMembers } from './features/cast/ListCastMembers';
 import { CreateCastMember } from './features/cast/CreateCastMember';
 import { EditCastMember } from './features/cast/EditCastMember';
@@ -19,48 +17,29 @@ import { ListVideo } from './features/videos/ListVideo';
 
 function App() {
   return (
-    <ThemeProvider theme={appTheme}>
-      <SnackbarProvider
-        maxSnack={3}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-      >
-        <Box 
-          component="main"
-          sx={{
-            height: '100vh',
-            backgroundColor: (theme) => theme.palette.grey[900],
-          }}
-        >
-          <Header />
-          <Layout>
-            <Routes>
-              <Route path='' element={<ListCategory />}/>
-              <Route path='/categories' element={<ListCategory />}/>
-              <Route path='/categories/create' element={<CreateCategory />}/>
-              <Route path='/categories/edit/:id' element={<EditCategory />}/>
-              <Route path='/cast-members' element={<ListCastMembers />} />
-              <Route path='/cast-members/create' element={<CreateCastMember />} />
-              <Route path='/cast-members/edit/:id' element={<EditCastMember />} />
-              <Route path='/genres' element={<ListGenre />} />
-              <Route path='/genres/create' element={<CreateGenre />} />
-              <Route path='/genres/edit/:id' element={<EditGenre />} />
-              <Route path='/videos' element={<ListVideo />} />
-              <Route path='/videos/create' element={<CreateVideo />} />
-              <Route path='/videos/edit/:id' element={<EditVideo />} />
-              <Route path='*' element={
-                <Box sx={{color: "white"}}>
-                  <Typography variant="h1">404</Typography>
-                  <Typography variant="h2">Page Not Found</Typography>
-                </Box>
-              }/>
-            </Routes>
-          </Layout>
-        </Box>
-      </SnackbarProvider>
-    </ThemeProvider>
+    <Layout>
+      <Routes>
+        <Route path='' element={<ListCategory />}/>
+        <Route path='/categories' element={<ListCategory />}/>
+        <Route path='/categories/create' element={<CreateCategory />}/>
+        <Route path='/categories/edit/:id' element={<EditCategory />}/>
+        <Route path='/cast-members' element={<ListCastMembers />} />
+        <Route path='/cast-members/create' element={<CreateCastMember />} />
+        <Route path='/cast-members/edit/:id' element={<EditCastMember />} />
+        <Route path='/genres' element={<ListGenre />} />
+        <Route path='/genres/create' element={<CreateGenre />} />
+        <Route path='/genres/edit/:id' element={<EditGenre />} />
+        <Route path='/videos' element={<ListVideo />} />
+        <Route path='/videos/create' element={<CreateVideo />} />
+        <Route path='/videos/edit/:id' element={<EditVideo />} />
+        <Route path='*' element={
+          <Box sx={{color: "white"}}>
+            <Typography variant="h1">404</Typography>
+            <Typography variant="h2">Page Not Found</Typography>
+          </Box>
+        }/>
+      </Routes>
+    </Layout>
   )
 }
 
